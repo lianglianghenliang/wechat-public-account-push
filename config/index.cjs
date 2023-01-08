3734ab92f61ecf1106cd83fabddc306468d046e0
lianglianghenliang <68846090+lianglianghenliang@users.noreply.github.com>
2023-01-08 11:58:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -12,44 +12,42 @@ const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '26ace7db37af0a51cf2831ef4847eca6',
-  PROVINCE: '上海',
-  CITY: '上海',
+  PROVINCE: '浙江',
+  CITY: '杭州',
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '妍妍小仙女',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ofkZC6WklR9_3ox-sdhvtWL_YI3c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'xMrZO1B1_HIdeZzNAvbSq9r7tWNX5kQjd5dPrGTAF48',
+      useTemplateId: 'tivMVWvEM4CxLqjCLssmXfW0n0wi0fkVoswnCQRFOWU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-05',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '妍妍小仙女', year: '1998', date: '11-05',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // {
+        //   type: '生日', name: '妍妍小仙女', year: '1998', date: '09-31',
+        // },
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '11-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-11-22' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'KUr6uNs-YVezkavnCZ6VrtW-M6eeWCoPhoeg43t7ovY',
+  CALLBACK_TEMPLATE_ID: '2dFMOUVWByV-KNkhnC53SUcqkSKVCjg9sldi-2B2CZQ',
 
   CALLBACK_USERS: [
     {
