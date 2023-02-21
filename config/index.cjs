f308248ed97daa2de173fc3ea936c6a786ef467d
lianglianghenliang <68846090+lianglianghenliang@users.noreply.github.com>
2023-02-21 10:24:36 +0800

    Update index.cjs
    加自己进去～

--- config/index.cjs
+++ config/index.cjs
@@ -43,6 +43,34 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2022-11-22' },
       ],
     },
+     {
+      // 想要发送的人的名字
+      name: '壮壮',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ofkZC6WklR9_3ox-sdhvtWL_YI3c',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'tivMVWvEM4CxLqjCLssmXfW0n0wi0fkVoswnCQRFOWU',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-03',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '大哥', year: '1998', date: '10-15',
+        },
+        // // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // {
+        //   type: '生日', name: '妍妍小仙女', year: '1998', date: '09-31',
+        // },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '11-22',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-11-22' },
+      ],
+    },
   ],
 
 
