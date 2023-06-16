83d35e79e17144dd249a013833698ca35f317a2d
lianglianghenliang <68846090+lianglianghenliang@users.noreply.github.com>
2023-06-16 10:02:42 +0800

    Update index.cjs
    模版替换

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ofkZC6Re8-M0yfIxl3JyebO7J-lM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'G86vhLFIVudbzt_nucFdMHprfo3nSsOUReJdsoEQl-g',
+      useTemplateId: 'tivMVWvEM4CxLqjCLssmXfW0n0wi0fkVoswnCQRFOWU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-05',
       festivals: [
